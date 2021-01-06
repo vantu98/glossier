b1b5b8fa6f190fb7e6aa784425848a6058e96987
tunv <nguyenvantu191298@gmail.com>
2021-01-06 10:30:26 +0700

    80% homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "primary-serif": ["Playfair Display"],
+      },
+      minHeight: {
+        "120px": "120px",
+        "135px": "135px",
+        "143px": "143px",
+      },
+    },
     container: {
       center: true,
+      padding: {},
     },
   },
   variants: {
